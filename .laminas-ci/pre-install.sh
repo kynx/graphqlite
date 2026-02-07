69710a5ee6547463a0466f1739adbd0b602aa3af
matt <matt@claritum.com>
2026-02-07 15:48:16 +0000

    Fix flag order

--- .laminas-ci/pre-install.sh
+++ .laminas-ci/pre-install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -e -o -x pipefail
+set -e -x -o pipefail
 
 GRAPHQLITE_VERSION="v0.3.2"
 GRAPHQLITE_SHA256=24f5bcf2f29f7f1cf3684cf812ed7c891d751d9103e1b82942f076a4b4b0a2e5
 
-WORKDIR="$2"
-JOB="$3"
+WORKDIR=$2
+JOB=$3
 COMMAND=$(echo "${JOB}" | jq -r '.command')
 if [[ ! ${COMMAND} =~ phpunit ]];then
     exit 0
