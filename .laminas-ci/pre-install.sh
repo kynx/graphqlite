a3a976843857d69481a35cfefb0df8bf5523b5af
matt <matt@claritum.com>
2026-02-07 15:36:34 +0000

    Gah. Don't set -u

--- .laminas-ci/pre-install.sh
+++ .laminas-ci/pre-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e -o -u -x pipefail
+set -e -o -x pipefail
 
 GRAPHQLITE_VERSION="v0.3.2"
 GRAPHQLITE_SHA256=24f5bcf2f29f7f1cf3684cf812ed7c891d751d9103e1b82942f076a4b4b0a2e5
