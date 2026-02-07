0c4e6627e9b6188f2c453b7cbf3a5aae18ea6be9
matt <matt@claritum.com>
2026-02-07 15:33:48 +0000

    Create phpunit.xml with extension path

--- .laminas-ci/pre-install.sh
+++ .laminas-ci/pre-install.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+set -e -o -u -x pipefail
 
 GRAPHQLITE_VERSION="v0.3.2"
 GRAPHQLITE_SHA256=24f5bcf2f29f7f1cf3684cf812ed7c891d751d9103e1b82942f076a4b4b0a2e5
 
 WORKDIR="$2"
+JOB="$3"
+COMMAND=$(echo "${JOB}" | jq -r '.command')
+if [[ ! ${COMMAND} =~ phpunit ]];then
+    exit 0
+fi
 
 export GRAPHQLITE_EXTENSION_PATH="${WORKDIR}/graphqlite.so"
 curl -L -o "${GRAPHQLITE_EXTENSION_PATH}" \
   "https://github.com/colliery-io/graphqlite/releases/download/${GRAPHQLITE_VERSION}/graphqlite-linux-x86_64.so"
 echo "${GRAPHQLITE_SHA256} ${GRAPHQLITE_EXTENSION_PATH}" | sha256sum -c
 
+cd "${WORKDIR}"
+sed -e "s#name=\"GRAPHQLITE_EXTENSION_PATH\" value=\"\"#name=\"GRAPHQLITE_EXTENSION_PATH\" value=\"${GRAPHQLITE_EXTENSION_PATH}\"#" \
+  phpunit.xml.dist > phpunit.xml
